ba5f2d10f882c87fcc2ab8d75681c2e5b2b50053
Renny Galindez <rfgalindez@gmail.com>
2021-06-11 21:22:10 -0500

    add reactQuery devtools

--- base-blog-em/src/contexts/react-query-config.js
+++ base-blog-em/src/contexts/react-query-config.js
@@ -1,10 +1,14 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
 const ReactQuery = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
