00dd8ec341aa5ef15cc4638529a62e0866098a36
Renny Galindez <rfgalindez@gmail.com>
2021-06-11 21:17:31 -0500

    use react-query to fecth posts

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { usePosts } from './hooks';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
-
-async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
-}
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isLoading } = usePosts();
+  if (isLoading) return <h1>loading...</h1>;
 
   return (
     <>
---
+++ base-blog-em/src/hooks/usePosts.js
@@ -0,0 +1,11 @@
+import { useQuery } from 'react-query';
+
+async function fetchPosts() {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0',
+  );
+  return response.json();
+}
+export default function usePosts() {
+  return useQuery('posts', fetchPosts);
+}
