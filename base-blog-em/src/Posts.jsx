f5de9e1d70bbb035d7a1ade0b04275de010df095
Renny Galindez <rfgalindez@gmail.com>
2021-06-13 01:30:23 -0500

    buttons (next, previous) activation.

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -5,11 +5,11 @@ import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isLoading } = usePosts();
+  const { data, isLoading } = usePosts(currentPage);
   if (isLoading) return <h1>loading...</h1>;
 
   return (
@@ -26,11 +26,17 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((currentPage) => --currentPage)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((currentPage) => ++currentPage)}
+        >
           Next page
         </button>
       </div>
