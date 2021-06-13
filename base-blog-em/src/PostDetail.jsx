d1bc14083550a21dd3617e061690bd9c7377c847
Renny Galindez <rfgalindez@gmail.com>
2021-06-13 03:54:35 -0500

    move updatePost function and use useUpdatePosr hook

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,29 +1,27 @@
-import { usePostComments, useDeletePost } from './hooks';
-
-async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } },
-  );
-  return response.json();
-}
+import { usePostComments, useDeletePost, useUpdatePost } from './hooks';
 
 export function PostDetail({ post }) {
   // replace with useQuery
   const { id } = post;
   const { data, isLoading } = usePostComments(id); //custom hook!
   const {
-    mutate,
+    mutate: mutateDelete,
     isError: isErrorDelete,
     isSuccess: isSuccessDelete,
     isLoading: isLoadingDelete,
   } = useDeletePost();
+  const {
+    mutate: mutateUpdate,
+    isError: isErrorUpdate,
+    isSuccess: isSuccessUpdate,
+    isLoading: isLoadingUpdate,
+  } = useUpdatePost();
 
   if (isLoading) return <h3> Loading...</h3>;
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button onClick={() => mutate(post.id)}>Delete</button>
+      <button onClick={() => mutateDelete(post.id)}>Delete</button>
       {isSuccessDelete && (
         <p style={{ color: 'green' }}>Post was not delete successfully</p>
       )}
@@ -34,7 +32,17 @@ export function PostDetail({ post }) {
         <p style={{ color: 'red' }}>Delete operation end with error!</p>
       )}
 
-      <button>Update title</button>
+      <button onClick={() => mutateUpdate(post.id)}>Update title</button>
+      {isSuccessUpdate && (
+        <p style={{ color: 'green' }}>Post was not Update successfully</p>
+      )}
+      {isLoadingUpdate && (
+        <p style={{ color: 'purple' }}>Procesing the Update operation...</p>
+      )}
+      {isErrorUpdate && (
+        <p style={{ color: 'red' }}>Update operation end with error!</p>
+      )}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
